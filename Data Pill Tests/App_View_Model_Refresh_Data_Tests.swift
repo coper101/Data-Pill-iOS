b4b457afabf289feb09002ceb0473c4a035738ca
Daryl <darylgialolo@gmail.com>
2023-03-21 17:21:42 +0800

    Fix: Refresh Mobile Data Test

--- Data Pill Tests/App_View_Model_Refresh_Data_Tests.swift
+++ Data Pill Tests/App_View_Model_Refresh_Data_Tests.swift
@@ -31,79 +31,7 @@ final class App_View_Model_Refresh_Data_Tests: XCTestCase {
         appViewModel.republishDataUsage()
 
         let expectation = expectation(description: "Get Updated Today's Data")
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            
-            appViewModel.refreshUsedDataToday(newTotalUsedData)
-            
-            expectation.fulfill()
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                
-                // (3) Then
-                let dateToday = appViewModel.todaysData.date
-                let dailyUsedDataToday = appViewModel.todaysData.dailyUsedData
-                let totalUsedDataToday = appViewModel.todaysData.totalUsedData
-                
-                XCTAssertNotNil(dateToday)
-                XCTAssertTrue(dateToday!.isToday())
-                XCTAssertEqual(dailyUsedDataToday, 300)
-                XCTAssertEqual(totalUsedDataToday, 400)
-                
-                expectation.fulfill()
-            }
-        }
-        
-        wait(for: [expectation], timeout: 1.5)
-    }
-    
-    func test_refresh_used_data_today_with_has_total_used_data() throws {
-        // (1) Given
-        let newTotalUsedData = 400.0
-        let existingTotalUsedData = 100.0
-        
-        // (2) When
-        let appViewModel = createAppViewModel(withTotalUsedData: existingTotalUsedData)
-        appViewModel.republishDataUsage()
-
-        let expectation = expectation(description: "Get Updated Today's Data")
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            
-            appViewModel.refreshUsedDataToday(newTotalUsedData)
-            
-            expectation.fulfill()
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                
-                // (3) Then
-                let dateToday = appViewModel.todaysData.date
-                let dailyUsedDataToday = appViewModel.todaysData.dailyUsedData
-                let totalUsedDataToday = appViewModel.todaysData.totalUsedData
-                
-                XCTAssertNotNil(dateToday)
-                XCTAssertTrue(dateToday!.isToday())
-                XCTAssertEqual(dailyUsedDataToday, 300)
-                XCTAssertEqual(totalUsedDataToday, 400)
-                
-                expectation.fulfill()
-            }
-        }
-        
-        
-        wait(for: [expectation], timeout: 1.5)
-    }
-    
-    func test_refresh_used_data_today_with_has_total_used_data_same() throws {
-        // (1) Given
-        let newTotalUsedData = 100.0
-        let existingTotalUsedData = 100.0
-        
-        // (2) When
-        let appViewModel = createAppViewModel(withTotalUsedData: existingTotalUsedData)
-        appViewModel.republishDataUsage()
-
-        let expectation = expectation(description: "Get Updated Today's Data")
+        let expectation2 = self.expectation(description: "Get Updated Today's Data After Refresh")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             
@@ -123,11 +51,85 @@ final class App_View_Model_Refresh_Data_Tests: XCTestCase {
                 XCTAssertEqual(dailyUsedDataToday, 0)
                 XCTAssertEqual(totalUsedDataToday, 100)
                 
-                expectation.fulfill()
+                expectation2.fulfill()
             }
         }
         
-        wait(for: [expectation], timeout: 1.5)
+        wait(for: [expectation, expectation2], timeout: 1.5)
+    }
+    
+    func test_refresh_used_data_today_with_has_total_used_data() throws {
+        // (1) Given
+        let newTotalUsedData = 400.0
+        let existingTotalUsedData = 100.0
+        
+        // (2) When
+        let appViewModel = createAppViewModel(withTotalUsedData: existingTotalUsedData)
+        appViewModel.republishDataUsage()
+
+        let expectation = self.expectation(description: "Get Updated Today's Data")
+        let expectation2 = self.expectation(description: "Get Updated Today's Data After Refresh")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            
+            appViewModel.refreshUsedDataToday(newTotalUsedData)
+            
+            expectation.fulfill()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                
+                // (3) Then
+                let dateToday = appViewModel.todaysData.date
+                let dailyUsedDataToday = appViewModel.todaysData.dailyUsedData
+                let totalUsedDataToday = appViewModel.todaysData.totalUsedData
+                
+                XCTAssertNotNil(dateToday)
+                XCTAssertTrue(dateToday!.isToday())
+                XCTAssertEqual(dailyUsedDataToday, 300)
+                XCTAssertEqual(totalUsedDataToday, 400)
+                
+                expectation2.fulfill()
+            }
+        }
+        
+        wait(for: [expectation, expectation2], timeout: 1.5)
+    }
+    
+    func test_refresh_used_data_today_with_has_total_used_data_same() throws {
+        // (1) Given
+        let newTotalUsedData = 100.0
+        let existingTotalUsedData = 100.0
+        
+        // (2) When
+        let appViewModel = createAppViewModel(withTotalUsedData: existingTotalUsedData)
+        appViewModel.republishDataUsage()
+
+        let expectation = self.expectation(description: "Get Updated Today's Data")
+        let expectation2 = self.expectation(description: "Get Updated Today's Data After Refresh")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            
+            appViewModel.refreshUsedDataToday(newTotalUsedData)
+            
+            expectation.fulfill()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                
+                // (3) Then
+                let dateToday = appViewModel.todaysData.date
+                let dailyUsedDataToday = appViewModel.todaysData.dailyUsedData
+                let totalUsedDataToday = appViewModel.todaysData.totalUsedData
+                
+                XCTAssertNotNil(dateToday)
+                XCTAssertTrue(dateToday!.isToday())
+                XCTAssertEqual(dailyUsedDataToday, 0)
+                XCTAssertEqual(totalUsedDataToday, 100)
+                
+                expectation2.fulfill()
+            }
+        }
+        
+        wait(for: [expectation, expectation2], timeout: 1.5)
     }
     
 
