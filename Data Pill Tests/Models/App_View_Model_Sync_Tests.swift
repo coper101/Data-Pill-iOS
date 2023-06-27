9fbe98227a05ead90d46c8a490daaddee0c5132c
coper101 <darylgialolo@gmail.com>
2023-06-27 22:31:05 +0800

    Add: App View Model Sync Unit Test Cases

--- Data Pill Tests/Models/App_View_Model_Sync_Tests.swift
+++ Data Pill Tests/Models/App_View_Model_Sync_Tests.swift
@@ -21,7 +21,10 @@ final class App_View_Model_Sync_Tests: XCTestCase {
         continueAfterFailure = false
     }
 
-    override func tearDownWithError() throws {}
+    override func tearDownWithError() throws {
+        localDatabase = nil
+        appViewModel = nil
+    }
     
     /// This deletes and create a new SQLite file of the Database
     func load_and_clear_local_database() throws {
@@ -44,7 +47,7 @@ final class App_View_Model_Sync_Tests: XCTestCase {
         #endif
     }
  
-    // MARK: Sync Old Data
+    // MARK: Sync Old Local Data
     func test_sync_old_local_then_remote_that_has_local_data_to_upload() throws {
         // (1) Given
         let todaysDate = Calendar.current.startOfDay(for: .init())
@@ -75,7 +78,7 @@ final class App_View_Model_Sync_Tests: XCTestCase {
             isSyncedToRemote: false, /// Data to Upload
             lastSyncedToRemoteDate: nil
         )
-        /// Override 2 Days Ago (if exists)
+        /// 2 Days Ago
         dataUsageRepository.addData(
             date: TestData.createDate(offset: -2, from: todaysDate),
             totalUsedData: 0,
@@ -104,6 +107,72 @@ final class App_View_Model_Sync_Tests: XCTestCase {
             XCTAssertEqual(oldData.count, 2)
             XCTAssertEqual(oldData[0].isSyncedToRemote, true)
             XCTAssertEqual(oldData[1].isSyncedToRemote, true)
+            
+            syncOldData.fulfill()
+        }
+        
+        wait(for: [syncOldData], timeout: 5.0)
+    }
+    
+    func test_sync_old_local_then_remote_that_has_local_data_to_upload_but_failed_to_login() throws {
+        // (1) Given
+        let todaysDate = Calendar.current.startOfDay(for: .init())
+        let lastSyncedToRemoteDate = TestData.createDate(offset: -2, from: todaysDate)
+        
+        let appDataRepository = AppDataRepository()
+        appDataRepository.setLastSyncedToRemoteDate(lastSyncedToRemoteDate)
+        
+        try load_and_clear_local_database()
+        let dataUsageRepository = DataUsageRepository(database: localDatabase)
+        let remoteDatabase = MockFailCloudDatabase()
+        
+        /// Today's Data
+        dataUsageRepository.addData(
+            date: todaysDate,
+            totalUsedData: 0,
+            dailyUsedData: 0,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: nil
+        )
+        /// Yesterday
+        dataUsageRepository.addData(
+            date: TestData.createDate(offset: -1, from: todaysDate),
+            totalUsedData: 0,
+            dailyUsedData: 1_500,
+            hasLastTotal: true,
+            isSyncedToRemote: false, /// Data to Upload
+            lastSyncedToRemoteDate: nil
+        )
+        /// 2 Days Ago
+        dataUsageRepository.addData(
+            date: TestData.createDate(offset: -2, from: todaysDate),
+            totalUsedData: 0,
+            dailyUsedData: 1_500,
+            hasLastTotal: true,
+            isSyncedToRemote: false, /// Data to Upload
+            lastSyncedToRemoteDate: nil
+        )
+        
+        appViewModel = .init(
+            appDataRepository: appDataRepository,
+            dataUsageRepository: dataUsageRepository,
+            dataUsageRemoteRepository: DataUsageRemoteRepository(remoteDatabase: remoteDatabase)
+        )
+        
+        // (2) When
+        appViewModel.syncOldThenRemoteData()
+        
+        // (3) Then
+       let syncOldData = XCTestExpectation(description: "Sync Old Data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let allData = dataUsageRepository.getAllData()
+            let oldData = allData.filter { $0.date != todaysDate }
+            
+            XCTAssertEqual(oldData.count, 2)
+            XCTAssertEqual(oldData[0].isSyncedToRemote, false)
+            XCTAssertEqual(oldData[1].isSyncedToRemote, false)
             
             syncOldData.fulfill()
         }
@@ -141,7 +210,7 @@ final class App_View_Model_Sync_Tests: XCTestCase {
             isSyncedToRemote: true,
             lastSyncedToRemoteDate: lastSyncedToRemoteDate /// Data to Update
         )
-        /// Override 2 Days Ago (if exists)
+        /// 2 Days Ago
         dataUsageRepository.addData(
             date: TestData.createDate(offset: -2, from: todaysDate),
             totalUsedData: 0,
@@ -180,9 +249,181 @@ final class App_View_Model_Sync_Tests: XCTestCase {
         
         wait(for: [syncOldData], timeout: 5.0)
     }
+    
+    func test_sync_old_local_then_remote_that_has_local_data_to_update_but_failed_to_login() throws {
+        // (1) Given
+        let todaysDate = Calendar.current.startOfDay(for: .init())
+        let lastSyncedToRemoteDate = TestData.createDate(offset: -2, from: todaysDate)
+        
+        let appDataRepository = AppDataRepository()
+        appDataRepository.setLastSyncedToRemoteDate(lastSyncedToRemoteDate)
+        
+        try load_and_clear_local_database()
+        let dataUsageRepository = DataUsageRepository(database: localDatabase)
+        let remoteDatabase = MockFailCloudDatabase()
+        
+        /// Today's Data
+        dataUsageRepository.addData(
+            date: todaysDate,
+            totalUsedData: 0,
+            dailyUsedData: 0,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: nil
+        )
+        /// Yesterday
+        dataUsageRepository.addData(
+            date: TestData.createDate(offset: -1, from: todaysDate),
+            totalUsedData: 0,
+            dailyUsedData: 1_500,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: lastSyncedToRemoteDate /// Data to Update
+        )
+        /// 2 Days Ago
+        dataUsageRepository.addData(
+            date: TestData.createDate(offset: -2, from: todaysDate),
+            totalUsedData: 0,
+            dailyUsedData: 1_500,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: lastSyncedToRemoteDate /// Data to Update
+        )
+        
+        appViewModel = .init(
+            appDataRepository: appDataRepository,
+            dataUsageRepository: dataUsageRepository,
+            dataUsageRemoteRepository: DataUsageRemoteRepository(remoteDatabase: remoteDatabase)
+        )
+        
+        // (2) When
+        appViewModel.syncOldThenRemoteData()
+        
+        // (3) Then
+       let syncOldData = XCTestExpectation(description: "Sync Old Data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            let allData = dataUsageRepository.getAllData()
+            let oldData = allData.filter { $0.date != todaysDate }
+            
+            XCTAssertEqual(oldData.count, 2)
+            
+            let firstDate = oldData[0].lastSyncedToRemoteDate!
+            XCTAssertNotEqual(Calendar.current.startOfDay(for: firstDate), todaysDate)
+            
+            let secondDate = oldData[1].lastSyncedToRemoteDate!
+            XCTAssertNotEqual(Calendar.current.startOfDay(for: secondDate), todaysDate)
+
+            syncOldData.fulfill()
+        }
+        
+        wait(for: [syncOldData], timeout: 5.0)
+    }
+    
+    // MARK: Sync Old Remote Data
+    func test_sync_old_local_then_remote_that_has_remote_data_to_download() throws {
+        // (1) Given
+        let todaysDate = Calendar.current.startOfDay(for: .init())
+        
+        try load_and_clear_local_database()
+        let dataUsageRepository = DataUsageRepository(database: localDatabase)
+        let remoteDatabase = MockSyncRemoteDatabase()
+        
+        /// Today's Data
+        dataUsageRepository.addData(
+            date: todaysDate,
+            totalUsedData: 0,
+            dailyUsedData: 0,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: nil
+        )
+        
+        appViewModel = .init(
+            dataUsageRepository: dataUsageRepository,
+            dataUsageRemoteRepository: DataUsageRemoteRepository(remoteDatabase: remoteDatabase)
+        )
+        
+        // (2) When
+        appViewModel.syncOldThenRemoteData()
+        
+        // (3) Then
+       let syncOldData = XCTestExpectation(description: "Sync Old Data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let allData = dataUsageRepository.getAllData()
+            let oldData = allData.filter { $0.date != todaysDate }
+            
+            XCTAssertEqual(oldData.count, 2)
+            XCTAssertEqual(oldData[0].isSyncedToRemote, true)
+            XCTAssertEqual(oldData[1].isSyncedToRemote, true)
+            
+            syncOldData.fulfill()
+        }
+        
+        wait(for: [syncOldData], timeout: 5.0)
+    }
+    
+    func test_sync_old_local_then_remote_that_has_remote_data_to_download_but_failed_to_login() throws {
+        // (1) Given
+        let todaysDate = Calendar.current.startOfDay(for: .init())
+        
+        try load_and_clear_local_database()
+        let dataUsageRepository = DataUsageRepository(database: localDatabase)
+        let remoteDatabase = MockFailCloudDatabase()
+        
+        /// Today's Data
+        dataUsageRepository.addData(
+            date: todaysDate,
+            totalUsedData: 0,
+            dailyUsedData: 0,
+            hasLastTotal: true,
+            isSyncedToRemote: true,
+            lastSyncedToRemoteDate: nil
+        )
+        
+        appViewModel = .init(
+            dataUsageRepository: dataUsageRepository,
+            dataUsageRemoteRepository: DataUsageRemoteRepository(remoteDatabase: remoteDatabase)
+        )
+        
+        // (2) When
+        appViewModel.syncOldThenRemoteData()
+        
+        // (3) Then
+       let syncOldData = XCTestExpectation(description: "Sync Old Data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let allData = dataUsageRepository.getAllData()
+            let oldData = allData.filter { $0.date != todaysDate }
+            
+            XCTAssertTrue(oldData.isEmpty)
+            
+            syncOldData.fulfill()
+        }
+        
+        wait(for: [syncOldData], timeout: 5.0)
+    }
+
 }
 
 final class MockSyncRemoteDatabase: RemoteDatabase {
+    
+    func fetchAll(of recordType: RecordType, recursively: Bool) -> AnyPublisher<[CKRecord], Error> {
+        let todaysDate = Calendar.current.startOfDay(for: .init())
+        return Just([
+            TestData.createDataRecord(
+                date: TestData.createDate(offset: -2, from: todaysDate),
+                dailyUsedData: 1_500 /// lower than 1500
+            ),
+            TestData.createDataRecord(
+                date: TestData.createDate(offset: -1, from: todaysDate),
+                dailyUsedData: 1_500 /// lower than 1500
+            )
+        ])
+        .setFailureType(to: Error.self)
+        .eraseToAnyPublisher()
+    }
     
     func fetch(with predicate: NSPredicate, of recordType: RecordType) -> AnyPublisher<[CKRecord], Error> {
         if recordType == .data {
