f64b289f38dfd80a4c4a9920b8534b2a249c2ba3
coper101 <darylgialolo@gmail.com>
2024-01-08 21:27:47 +0800

    Update: Disable iCloud Change Notifications

--- Data Pill/App/AppDelegate.swift
+++ Data Pill/App/AppDelegate.swift
@@ -16,46 +16,47 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions
         launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
-        UIApplication.shared.registerForRemoteNotifications()
-        Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 \(UIApplication.shared.isRegisteredForRemoteNotifications ? "Registered" : "Not Registered")")
+        /// Disable iCloud for now
+        // UIApplication.shared.registerForRemoteNotifications()
+        // Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 \(UIApplication.shared.isRegisteredForRemoteNotifications ? "Registered" : "Not Registered")")
         return true
     }
     
-    func application(
-        _ application: UIApplication,
-        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
-        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-    ) {
-        Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 Received")
-        
-        guard
-            let notification = CKNotification(fromRemoteNotificationDictionary: userInfo),
-            let subscriptionID =  notification.subscriptionID,
-            let remoteSubscription = RemoteSubscription(rawValue: subscriptionID)
-        else {
-            return
-        }
-        
-        switch remoteSubscription {
-        case .plan:
-            NotificationCenter.default.post(name: .plan, object: nil)
-        case .todaysData:
-            NotificationCenter.default.post(name: .todaysData, object: nil)
-        }
-        
-        completionHandler(.newData)
-    }
+    // func application(
+    //     _ application: UIApplication,
+    //     didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+    //     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    // ) {
+    //     Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 Received")
+    //
+    //     guard
+    //         let notification = CKNotification(fromRemoteNotificationDictionary: userInfo),
+    //         let subscriptionID =  notification.subscriptionID,
+    //         let remoteSubscription = RemoteSubscription(rawValue: subscriptionID)
+    //     else {
+    //         return
+    //     }
+    //
+    //     switch remoteSubscription {
+    //     case .plan:
+    //         NotificationCenter.default.post(name: .plan, object: nil)
+    //     case .todaysData:
+    //         NotificationCenter.default.post(name: .todaysData, object: nil)
+    //     }
+    //
+    //     completionHandler(.newData)
+    // }
     
-    private func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        /// Note: Most of the time, this method is not called
-        Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 Registered")
-    }
+    // private func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    //     /// Note: Most of the time, this method is not called
+    //     Logger.appDelegate.debug("- REMOTE NOTIFICATION: 💈 Registered")
+    // }
     
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-        /// Note: Most of the time, this method is not called
-        Logger.appDelegate.debug("- APP DELEGATE: ℹ️ Failed to Register | REASON: \(error.localizedDescription)")
-    }
+    // func application(
+    //     _ application: UIApplication,
+    //     didFailToRegisterForRemoteNotificationsWithError error: Error
+    // ) {
+    //     /// Note: Most of the time, this method is not called
+    //     Logger.appDelegate.debug("- APP DELEGATE: ℹ️ Failed to Register | REASON: \(error.localizedDescription)")
+    // }
 }
