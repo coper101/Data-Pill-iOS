d56c0b575a2187001356631ac7f8e9f4d3227e9f
coper101 <darylgialolo@gmail.com>
2024-01-17 22:53:28 +0800

    Add: #74 Settings Screen Items

---
+++ Data Pill/Views/SettingsRowView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsRowView.swift
+//  Data Pill
+//
+//  Created by Wind Versi on 17/1/24.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    // MARK: - Props
+    
+    // MARK: - UI
+    var body: some View {
+        VStack(spacing: 0) {
+            // TODO:
+        }
+    }
+    
+    // MARK: - Actions
+}
+
+// MARK: - Preview
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView()
+            .previewLayout(.sizeThatFits)
+            // .background(Colors.Background)
+    }
+}
---
+++ Data Pill/Views/SettingsView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsView.swift
+//  Data Pill
+//
+//  Created by Wind Versi on 13/1/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - Props
+    
+    // MARK: - UI
+    var body: some View {
+        VStack(spacing: 0) {
+            // TODO:
+        }
+    }
+    
+    // MARK: - Actions
+}
+
+// MARK: - Preview
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .previewLayout(.sizeThatFits)
+            // .background(Colors.Background)
+    }
+}
